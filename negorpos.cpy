4ed05400711aad4766c757922b6a59e82a77bf13
Yusuf Ali <135424583+Zerzahpromax@users.noreply.github.com>
2023-11-21 17:19:07 +0530

    Add files via upload

---
+++ negorpos.cpy
@@ -0,0 +1,5 @@
+a=int(input("Enter a number"))
+if(a>0):
+    print("POSTIVE")
+else:
+    print("NEGATIVE")
---
+++ vote.py
@@ -0,0 +1,5 @@
+age=int(input("Enter your age"))
+if(age>=18):
+    print("Eligible to VOTE")
+else:
+    print("Not Eligible to VOTE")
